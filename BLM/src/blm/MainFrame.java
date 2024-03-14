f8987435b8cf61a5b25bb12fc4c61e60c9e95e01
Dssmirnov-msc <61143611+Dssmirnov-msc@users.noreply.github.com>
2024-03-14 11:28:23 +0300

    Добавили Кнопку
    Привет, Мир!

--- BLM/src/blm/MainFrame.java
+++ BLM/src/blm/MainFrame.java
@@ -27,21 +27,41 @@ public class MainFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        HellloWorldButton = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        HellloWorldButton.setText("Hello World");
+        HellloWorldButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                HellloWorldButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(98, 98, 98)
+                .addComponent(HellloWorldButton, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(125, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(78, 78, 78)
+                .addComponent(HellloWorldButton, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(138, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void HellloWorldButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_HellloWorldButtonActionPerformed
+        System.out.println("Hello, World");
+        
+    }//GEN-LAST:event_HellloWorldButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +99,6 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton HellloWorldButton;
     // End of variables declaration//GEN-END:variables
 }
