0b90d1b9e41c6149b8f9c391790f8b0b12e75eb8
Dssmirnov-msc <61143611+Dssmirnov-msc@users.noreply.github.com>
2024-03-14 11:31:27 +0300

    Фрейм видимый
    Добавлен экземпляр MainFrame и установлен видимым

--- BLM/src/blm/BLM.java
+++ BLM/src/blm/BLM.java
@@ -15,7 +15,8 @@ public class BLM {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MainFrame mn = new MainFrame();
+        mn.setVisible(true);
     }
     
 }
